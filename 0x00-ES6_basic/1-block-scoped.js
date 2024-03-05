8cc92bde25161be8144599afbdb8210ee4484ae4
john mwanyambu <jmwanyambu@gmail.com>
2024-03-05 08:38:07 +0000

    es6

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,9 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
     let task = true;
+    // eslint-disable-next-line no-unused-vars
     let task2 = false;
   }
 
