2fc591752d4fcd460ff113cb5f45b80cf82064ef
john mwanyambu <jmwanyambu@gmail.com>
2024-03-05 08:48:10 +0000

    block scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,11 @@
-export default function taskBlock() {
-  let task = false;
-  let task2 = true;
+export default function taskBlock(trueOrFalse) {
+  var task = false; 
+  var task2 = true;
+
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false; 
+  }
 
   return [task, task2];
 }
