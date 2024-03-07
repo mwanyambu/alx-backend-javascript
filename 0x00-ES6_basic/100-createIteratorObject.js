ea9ac049575f41906cf6e117f21ccfe16850d35a
john mwanyambu <jmwanyambu@gmail.com>
2024-03-07 22:22:40 +0000

    import fix

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,11 +1,19 @@
 export default function* createIteratorObject(report) {
-  const { allEmployees } = report;
+  if (!report || !report.allEmployees || typeof report.allEmployees !== 'object') {
+    throw new Error('Invalid report object');
+  }
 
-  for (const department in allEmployees) {
-    const employeesInDepartment = allEmployees[department];
+  for (const department in report.allEmployees) {
+    if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+      const employeesInDepartment = report.allEmployees[department];
 
-    for (const employee of employeesInDepartment) {
-      yield employee;
+      if (!Array.isArray(employeesInDepartment)) {
+        throw new Error(`Invalid employee list for department: ${department}`);
+      }
+
+      for (const employee of employeesInDepartment) {
+        yield employee;
+      }
     }
   }
 }
