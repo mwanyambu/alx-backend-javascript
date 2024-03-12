ff6d62c9352e7758321d3794db8242ab84561857
john mwanyambu <jmwanyambu@gmail.com>
2024-03-12 19:13:54 +0000

    student list

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,11 @@
 export default function getListStudents () {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-  ]
+  const lst1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco'
+  };
+  const lst2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const lst3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+
+  return [lst1, lst2, lst3]
 }
