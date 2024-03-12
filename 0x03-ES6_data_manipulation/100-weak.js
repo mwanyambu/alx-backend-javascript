372719e27d6b57979b91a999fcdfd1783948a66e
john mwanyambu <jmwanyambu@gmail.com>
2024-03-12 20:05:47 +0000

    endpoint

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -8,6 +8,6 @@ export function queryAPI(endpoint) {
   }
   weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
-    throw new Error('Endpointload is high');
+    throw new Error('Endpoint load is high');
   }
 }
