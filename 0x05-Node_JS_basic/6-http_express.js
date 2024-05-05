53bdbac1a4f534d022939a9add0bad97b0ed47d6
mwanyambu <jmwanyambu@gmail.com>
2024-05-05 12:46:22 +0300

    6http

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 1245;
 
 app.get('/', (_, res) => {
-  res.send('Hello Holberton School');
+  res.send('Hello Holberton School!');
 });
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
